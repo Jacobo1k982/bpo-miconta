e43c501a30b4592aba405fc2d30b7d1cb090e116
Jacobo1k982 <askmecr@gmail.com>
2025-06-25 23:36:55 -0600

    v13

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // ✅ ESTA LÍNEA ES CLAVE
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css"; // tu archivo de estilos globales
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
